f1cc46755321fbdf5bdc79136e3b950831029244
mila221 <mila.cvetkovic99@gmail.com>
2020-12-21 09:15:35 +0100

    fajlovi dodati.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function dark(){
+    $(".glavni-col").css("background-color", "#111");
+    $("body").css("background-color", "#111");
+    $("#glavni").css("background", "#111");
+    $("p").css("color", "white");
+    $("a").css("color", "white");
+    $("h3").css("color", "white");
+
+
+
+}
